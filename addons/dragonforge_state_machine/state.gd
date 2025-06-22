45b2ff7816ddde059b001a398355d51c1c146e43
Dragonforge Development <dougbnoel@gmail.com>
2025-06-22 14:43:52 -0600

    Version 0.1.1: Fixed race condition when calling auit() for the game.

--- addons/dragonforge_state_machine/state.gd
+++ addons/dragonforge_state_machine/state.gd
@@ -15,6 +15,8 @@ class_name State extends Node
 var can_transition = true
 # A reference to the state machine used for switching states.
 var _state_machine: StateMachine
+# Used when deactivating a state and the [StateMachine] has already been deleted.
+var _state_machine_name: String
 # The name of the parent node of the state_machine. Stored for logging purposes.
 # NOTE: This is not guaranteed to be the same as get_owner().name
 var _owner_name: String
@@ -47,8 +49,9 @@ func is_current_state() -> bool:
 ## [br][i]Never[/i] call this method directly. It should only be used by the [StateMachine]
 func _activate_state() -> void:
 	_state_machine = get_parent()
+	_state_machine_name = _state_machine.name
 	_owner_name = _state_machine.get_parent().name
-	print_rich("[color=forest_green][b]Activate[/b][/color] [color=gold][b]%s[/b][/color] [color=ivory]%s State:[/color] %s" % [_owner_name, _state_machine.name, self.name])
+	print_rich("[color=forest_green][b]Activate[/b][/color] [color=gold][b]%s[/b][/color] [color=ivory]%s State:[/color] %s" % [_owner_name, _state_machine_name, self.name])
 
 
 ## Called when a [State] is removed from a [StateMachine].
@@ -56,7 +59,7 @@ func _activate_state() -> void:
 ## [br]When overriding, be sure to call [method super] on the first line of your method.
 ## [br][i]Never[/i] call this method directly. It should only be used by the [StateMachine]
 func _deactivate_state() -> void:
-	print_rich("[color=#d42c2a][b]Deactivate[/b][/color] [color=gold][b]%s[/b][/color] [color=ivory]%s State:[/color] %s" % [_owner_name, _state_machine.name, self.name])
+	print_rich("[color=#d42c2a][b]Deactivate[/b][/color] [color=gold][b]%s[/b][/color] [color=ivory]%s State:[/color] %s" % [_owner_name, _state_machine_name, self.name])
 
 
 ## Called every time the [State] is entered.
@@ -64,7 +67,7 @@ func _deactivate_state() -> void:
 ## [br]When overriding, be sure to call [method super] on the first line of your method.
 ## [br][i]Never[/i] call this method directly. It should only be used by the [StateMachine]
 func _enter_state() -> void:
-	print_rich("[color=deep_sky_blue][b]Enter[/b][/color] [color=gold][b]%s[/b][/color] [color=ivory]%s State:[/color] %s" % [_owner_name, _state_machine.name, self.name])
+	print_rich("[color=deep_sky_blue][b]Enter[/b][/color] [color=gold][b]%s[/b][/color] [color=ivory]%s State:[/color] %s" % [_owner_name, _state_machine_name, self.name])
 
 
 ## Called every time the [State] is exited.
@@ -72,4 +75,4 @@ func _enter_state() -> void:
 ## [br]When overriding, be sure to call [method super] on the first line of your method.
 ## [br][i]Never[/i] call this method directly. It should only be used by the [StateMachine]
 func _exit_state() -> void:
-	print_rich("[color=dark_orange][b]Exit[/b][/color] [color=gold][b]%s[/b][/color] [color=ivory]%s State:[/color] %s" % [_owner_name, _state_machine.name, self.name])
+	print_rich("[color=dark_orange][b]Exit[/b][/color] [color=gold][b]%s[/b][/color] [color=ivory]%s State:[/color] %s" % [_owner_name, _state_machine_name, self.name])
